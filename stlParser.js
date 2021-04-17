469b38d7bb5453b308f4c7fd7f1fb70662ba1737
Andrew Lyon <ZUI=973z>
2021-04-17 13:00:18 -0600

    fixed logic & cleaned up code

--- stlParser.js
+++ stlParser.js
@@ -1,18 +1,22 @@
 var fs = require("fs"), filename = process.argv[2];
 
 fs.readFile(filename, "utf8", function(err, data) {
-    let loops = data.match(/outer loop[\s\S]*?endloop/g)
+    if(err?.code) {
+        console.error('Error loading file');
+        return;
+    }
+
+    let loops = data.match(/outer loop[\s\S]*?endloop/g);
     let surfaceArea = 0;
 
-    
-    console.log('Number of Triangles: ', loops.length);
     const vertices = extractVertices(loops);
     
     vertices.map((innerVertices) => {
         surfaceArea += determineTriangleArea(innerVertices);
     })
-
-    console.log('Surface Area: ', surfaceArea.toFixed(4));
+    
+    console.log('Number of Triangles: ', loops.length + 
+                '\nSurface Area: ', surfaceArea.toFixed(4));
 });
 
 const extractVertices = (loops) => {
@@ -30,28 +34,22 @@ const extractVertices = (loops) => {
     while (vertices.length > 0) {
         verticesFinal.push(vertices.splice(0,3));
     }
-
     return verticesFinal;
 } 
 
 const determineTriangleArea = (triangle) => {
-
     const sideA = determineLengthOfSide(triangle[0], triangle[1]);   
     const sideB = determineLengthOfSide(triangle[0], triangle[2]);
     const sideC = determineLengthOfSide(triangle[1], triangle[2]);
 
     const semiperimeter = (sideA + sideB + sideC) / 2
 
-    const x = Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
-    // console.log(x);
-    return x;   
+    return Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC)); 
 }
 
 const determineLengthOfSide = (point1, point2) => {
-
     var newPoint = point1.map((item, index) => {
-        return item - point2[index]
+        return Math.pow(item - point2[index], 2);
     })
-
-    return Math.sqrt(Math.abs(newPoint.reduce((a, b) => a + b, 0)));
+    return Math.sqrt(newPoint.reduce((a, b) => a + b, 0));
 }
